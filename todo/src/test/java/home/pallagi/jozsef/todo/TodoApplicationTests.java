68da711ee4a8dc12cd685d1c65057eb45355addd
PallagiJoe <Pallagi.Jozsef@nexius.hu>
2024-11-22 13:15:51 +0100

    create app

---
+++ todo/src/test/java/home/pallagi/jozsef/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package home.pallagi.jozsef.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
