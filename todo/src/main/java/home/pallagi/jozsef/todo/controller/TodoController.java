110095c74466d1c1caa8eda7d0bd1bf49b2c6b19
PallagiJoe <Pallagi.Jozsef@nexius.hu>
2024-11-25 08:18:50 +0100

    Add exception handling for validation errors in TodoController

--- todo/src/main/java/home/pallagi/jozsef/todo/controller/TodoController.java
+++ todo/src/main/java/home/pallagi/jozsef/todo/controller/TodoController.java
@@ -1,9 +1,15 @@
 package home.pallagi.jozsef.todo.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import home.pallagi.jozsef.todo.entity.Todo;
@@ -37,12 +44,26 @@ public class TodoController {
 
     @PatchMapping
     Todo update(Todo todo) {
-       return this.todoService.update(todo);
+        return this.todoService.update(todo);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") Long id) {
         this.todoService.delete(id);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            // String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put("message", errorMessage);
+        });
+        System.out.println(errors);
+        return errors;
     }
 
 }
