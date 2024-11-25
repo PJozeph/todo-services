b92d6dd0daddce9f67f61e89a2cb9f59e0bb5f3e
PallagiJoe <Pallagi.Jozsef@nexius.hu>
2024-11-25 10:06:17 +0100

    Add database schema and initial data for labels and todo-label relationships

--- todo/src/main/resources/data.sql
+++ todo/src/main/resources/data.sql
@@ -1,3 +1,9 @@
 INSERT INTO todo (title, description, completed, deadline) VALUES 
 ('Buy Milk', 'Description for task 1', false, '2024-11-25'),
 ('Clean House', 'Description for task 2', true, '2024-11-30');
+
+INSERT INTO label (title) VALUES ('Home'), ('Work');
+
+INSERT INTO todo_label (todo_id, label_id) VALUES 
+(1, 1),
+(2, 2);
