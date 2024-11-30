a4b507f04eb42f308a430cad00d7830ce088593c
PallagiJoe <Pallagi.Jozsef@nexius.hu>
2024-11-30 13:56:03 +0100

    Add validation test for Todo title to ensure it cannot be null

--- src/test/java/home/pallagi/jozsef/todo/service/TodoServiceUnitTests.java
+++ src/test/java/home/pallagi/jozsef/todo/service/TodoServiceUnitTests.java
@@ -6,14 +6,22 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.BDDMockito.given;
+
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import home.pallagi.jozsef.todo.entity.Todo;
 import home.pallagi.jozsef.todo.repository.TodoRepository;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 
 @ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -27,12 +35,17 @@ public class TodoServiceUnitTests {
 
     private Todo todo;
 
+    private Validator validator;
+
     @BeforeEach
     public void setup() {
         todo = new Todo();
         todo.setId(1L);
         todo.setTitle("Test Todo");
         todo.setDescription("Test Description");
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
     }
 
     @Test
@@ -43,6 +56,17 @@ public class TodoServiceUnitTests {
         Todo savedTodo = todoService.save(todo);
 
         assertThat(savedTodo).isNotNull();
+    }
+
+    @Test
+    @Order(2)
+    public void throwExceptionWhenTodoTitleIsNull() {
+        Todo todo = new Todo();
+        todo.setTitle(null);
+
+        Set<ConstraintViolation<Todo>> violations = validator.validate(todo);
+
+        assertThat(violations.size()).isEqualTo(1);
     }
 
 }
