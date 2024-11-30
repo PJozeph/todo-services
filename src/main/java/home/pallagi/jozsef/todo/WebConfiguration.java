ecf668fb468a73a5d52559e9f6a2f95f3665abfb
PallagiJoe <Pallagi.Jozsef@nexius.hu>
2024-11-30 14:15:47 +0100

    Rename WebConfiguration class package for improved structure

--- src/main/java/home/pallagi/jozsef/todo/WebConfiguration.java
+++ src/main/java/home/pallagi/jozsef/todo/WebConfiguration.java
@@ -1,4 +1,4 @@
-package home.pallagi.jozsef.todo.config;
+package home.pallagi.jozsef.todo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
