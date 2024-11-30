62458cfbaf3e6b4f89e8a38f37ea1550d65b206c
PallagiJoe <Pallagi.Jozsef@nexius.hu>
2024-11-30 14:42:48 +0100

    Refine CORS configuration to specify allowed origins and enhance security settings

--- src/main/java/home/pallagi/jozsef/todo/WebConfiguration.java
+++ src/main/java/home/pallagi/jozsef/todo/WebConfiguration.java
@@ -2,17 +2,23 @@ package home.pallagi.jozsef.todo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         System.out.println("CORS enabled");
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://todo-service-681005455667.us-central1.run.app", // Your app domain
+                        "http://localhost:8080", // Local Swagger UI for testing
+                        "https://editor.swagger.io" // Optional: for using Swagger Editor
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
-    
+
 }
