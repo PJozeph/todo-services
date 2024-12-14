7ae12c35d6f4cd9251f80c935f626c12bc0e0e4a
PallagiJoe <Pallagi.Jozsef@nexius.hu>
2024-12-14 17:51:24 +0100

    set up JWT

--- src/main/java/home/pallagi/jozsef/todo/security/services/UserDetailsImpl.java
+++ src/main/java/home/pallagi/jozsef/todo/security/services/UserDetailsImpl.java
@@ -1,14 +1,14 @@
 package home.pallagi.jozsef.todo.security.services;
 
-import lombok.Builder;
+import home.pallagi.jozsef.todo.entity.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-@Builder
 public class UserDetailsImpl implements UserDetails {
 
     private Long id;
@@ -16,6 +16,21 @@ public class UserDetailsImpl implements UserDetails {
     private String email;
     @JsonIgnore
     private String password;
+
+    public UserDetailsImpl(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword());
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
--- src/main/java/home/pallagi/jozsef/todo/security/services/UserDetailsServiceImpl.java
+++ src/main/java/home/pallagi/jozsef/todo/security/services/UserDetailsServiceImpl.java
@@ -1,16 +1,26 @@
 package home.pallagi.jozsef.todo.security.services;
 
+import home.pallagi.jozsef.todo.entity.User;
+import home.pallagi.jozsef.todo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-//    @Autowired
-//    UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
     }
 }
